c99f0b80b11f1f588db109594e7e22b22489cae8
surajung <hdoc79@gmail.com>
2023-06-09 09:57:14 +0900

    feat: 더미데이터 수정 - 카테고리 상수화, 이에 따른 수정

---
+++ utils/constants.ts
@@ -0,0 +1,79 @@
+const codes = {
+  marketItemCategory: {
+    depth1: [
+      {
+        code: "c1",
+        codeName: "America",
+      },
+      {
+        code: "c2",
+        codeName: "Asia",
+      },
+      {
+        code: "c3",
+        codeName: "Europe",
+      },
+    ],
+    depth2: [
+      {
+        code: "c01",
+        codeName: "smartphones",
+      },
+      {
+        code: "c02",
+        codeName: "laptops",
+      },
+      {
+        code: "c03",
+        codeName: "skincare",
+      },
+      {
+        code: "c04",
+        codeName: "smartphones",
+      },
+      {
+        code: "c05",
+        codeName: "laptops",
+      },
+      {
+        code: "c06",
+        codeName: "skincare",
+      },
+    ],
+    depth3: [
+      {
+        code: "c001",
+        codeName: "Apple",
+      },
+      {
+        code: "c002",
+        codeName: "Samsung",
+      },
+      {
+        code: "c003",
+        codeName: "Huawei",
+      },
+      {
+        code: "c004",
+        codeName: "Microsoft",
+      },
+      {
+        code: "c005",
+        codeName: "ABC",
+      },
+      {
+        code: "c006",
+        codeName: "DEF",
+      },
+      {
+        code: "c007",
+        codeName: "GHI",
+      },
+      {
+        code: "c008",
+        codeName: "JKL",
+      },
+    ],
+  },
+};
+export default Object.freeze(codes);
