4a334c649305cc14471267a2e4b364c24ee37e3b
Cho Sung chang <hdoc79@gmail.com>
2023-06-11 13:40:47 +0900

    feat:  검색결과페이지 개발

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, shrink-to-fit=no"></meta>
+      </Head>
       <Component {...pageProps} />
     </>
   );
--- pages/search.tsx
+++ pages/search.tsx
@@ -1,8 +1,25 @@
-import {useState} from "react"
+import {useState,useEffect} from "react"
 import DefaultLayout from "@/components/layout/DefaultLayout"
 import TextInput from "@/components/TextInput/TextInput"
+import MarketItem from "@/components/MarketItem/MarketItem";
+import Modal from "@/components/Modal/Modal";
+import CategorySelect from "@/components/CategorySelect/CategorySelect";
+import Button from "@/components/Button/Button";
+import { MarketItemType } from "@/utils/types";
+
 const Search = () => {
   const [keyword, setKeyword] = useState("");
+  const [isModal, setIsModal] = useState(false);
+  const [marketItemList, setMarketItemList] = useState<MarketItemType[]>([]);
+
+  const toggleModal = () => {
+    setIsModal(prev => !prev)
+  }
+  useEffect(() => {
+    fetch("/api/market")
+      .then((response) => response.json())
+      .then((response) => setMarketItemList(response.result));
+  }, []);
   return (
     <DefaultLayout>
       <div className="search">
@@ -15,18 +32,52 @@ const Search = () => {
             maxLength={30}
             onChange={(e) => setKeyword(e.target.value)}
           ></TextInput>
-          <button className="button__cart" type="button">
+          <button className="button__cart-page" type="button">
             <span className="blind">장바구니</span>
           </button>
         </div>
-        <ul className="search__autocorrect">
-          <li>
-            <span className="point">TEXT</span>
-            test
-            <button className="search__autocorrect--select" type="button"><span className="blind">선택</span></button>
-          </li>
-        </ul>
+        {false && (
+          <ul className="search__autocorrect">
+            <li>
+              <span className="point">TEXT</span>
+              test
+              <button className="search__autocorrect--select" type="button"><span className="blind">선택</span></button>
+            </li>
+          </ul>
+        )}
+        <div className="search__filter">
+          <div className="search__filter--keyword"></div>
+          <button className="search__filter--button" type="button" onClick={() => toggleModal()}><span className="blind">카테고리</span></button>
+        </div>
+        <div className="search__list">
+          <ul className="inner">
+          {marketItemList.map((item) => (
+            <li>
+              <MarketItem
+                id={item.id}
+                title={item.title}
+                description={item.description}
+                thumbnail={item.thumbnail}
+                price={item.price}
+                discountPercentage={item.discountPercentage}
+                key={item.id}
+              />
+              <button className="button__cart" type="button">
+                <span className="blind">장바구니에 담기</span>
+              </button>
+            </li>
+          ))}
+          </ul>
+        </div>
       </div>
+      {isModal && (
+        <Modal title="제목" height="500px" onHandler={() => toggleModal()}>
+          <CategorySelect />
+          <Button variant="primary_filled" size="h56" isFullWidth={true}>
+            선택하기
+          </Button>
+        </Modal>
+        )}
     </DefaultLayout>
   )
 }
